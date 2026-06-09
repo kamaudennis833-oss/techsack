7e639be0d6f21ac55c24147a4a676369e67bf1fd
kamaudennis833-oss <kamaudennis833@gmail.com>
2026-06-09 16:08:26 +0300

    Remove database connection code from db.php

--- db.php
+++ db.php
@@ -1,13 +1,1 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "denis2569";
-$db = "lms";
-$port = 3307; 
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
